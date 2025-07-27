2d461b40c102a6e9ef7ab7574721601c2338140f
lullabyX <hassan_rabbi@live.com>
2025-07-27 19:12:41 +0600

    ui: mobile friendly

--- packages/components/src/site-header/index.tsx
+++ packages/components/src/site-header/index.tsx
@@ -6,7 +6,7 @@ import ThemeSwitcher from '../theme/theme-switcher';
 import Link from 'next/link';
 import { Button } from '@hey-world/ui';
 import { usePathname } from 'next/navigation';
-import { Github } from 'lucide-react';
+import { Github, Menu, X } from 'lucide-react';
 
 interface SiteHeaderProps extends React.HTMLAttributes<HTMLElement> {
   title: string;
@@ -25,6 +25,7 @@ const SiteHeader: React.FC<SiteHeaderProps> = ({
   ...props
 }) => {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const pathname = usePathname();
 
   useEffect(() => {
@@ -40,27 +41,31 @@ const SiteHeader: React.FC<SiteHeaderProps> = ({
     };
   }, []);
 
+  useEffect(() => {
+    setIsMenuOpen(false);
+  }, [pathname]);
+
   return (
     <header
       className={cn(
         'sticky top-0 z-50 flex w-full items-center justify-center bg-transparent transition-all duration-300 ease-in-out',
-        isScrolled ? 'top-2 py-2' : 'py-4',
+        isScrolled && !isMenuOpen ? 'top-2 py-2' : 'py-4',
         className
       )}
       {...props}
     >
       <div
         className={cn(
-          'flex h-16 items-center justify-between bg-background/80 px-6 transition-all duration-300 ease-in-out',
-          isScrolled
-            ? 'border-slate w-3/4 max-w-7xl rounded-full border shadow-sm'
+          'relative flex h-12 items-center justify-between bg-background/80 px-4 transition-all duration-300 ease-in-out md:h-16 md:px-6',
+          isScrolled && !isMenuOpen
+            ? 'border-slate w-80 max-w-7xl rounded-full border shadow-sm md:w-3/4'
             : 'w-full max-w-full rounded-none border border-transparent'
         )}
       >
         <Button
           asChild
           variant="link"
-          className="text-lg font-bold hover:no-underline"
+          className="text-base font-bold hover:no-underline md:text-lg"
         >
           <Link
             href={titleHref}
@@ -73,7 +78,7 @@ const SiteHeader: React.FC<SiteHeaderProps> = ({
             {title}
           </Link>
         </Button>
-        <nav className="flex space-x-4">
+        <nav className="hidden space-x-4 md:flex">
           {pages?.map((page) => (
             <Button key={page.name} asChild variant="link">
               <Link
@@ -91,17 +96,56 @@ const SiteHeader: React.FC<SiteHeaderProps> = ({
         </nav>
         <div className="flex items-center">
           <Button variant="ghost" size="icon" asChild>
-            <a
+            <Link
               href="https://github.com/lullabyX/hey-world"
               target="_blank"
               rel="noopener noreferrer"
             >
               <Github className="h-5 w-5" />
               <span className="sr-only">GitHub</span>
-            </a>
+            </Link>
           </Button>
           <ThemeSwitcher />
+          <Button
+            variant="ghost"
+            size="icon"
+            className="md:hidden"
+            onClick={() => setIsMenuOpen((prev) => !prev)}
+          >
+            {isMenuOpen ? (
+              <X className="h-5 w-5" />
+            ) : (
+              <Menu className="h-5 w-5" />
+            )}
+            <span className="sr-only">Toggle menu</span>
+          </Button>
         </div>
+        {isMenuOpen && (
+          <div className="fixed inset-x-0 top-16 z-50 h-[calc(100vh-3rem)] overflow-y-auto bg-background p-4 px-6 shadow-md md:hidden">
+            <nav className="flex flex-col space-y-2">
+              {pages?.map((page) => (
+                <Button
+                  key={page.name}
+                  variant="link"
+                  className="w-min justify-start rounded-md"
+                  asChild
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  <Link
+                    href={page.href}
+                    onClick={() => {
+                      if (page.href === pathname) {
+                        window.scrollTo({ top: 0, behavior: 'smooth' });
+                      }
+                    }}
+                  >
+                    {page.name}
+                  </Link>
+                </Button>
+              ))}
+            </nav>
+          </div>
+        )}
       </div>
     </header>
   );
