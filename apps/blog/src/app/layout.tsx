9c99686f74d5714b11d2980c561524a71c39324a
lullabyX <hassan_rabbi@live.com>
2025-07-27 17:57:44 +0600

    add blog

---
+++ apps/blog/src/app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from 'next';
+import '@hey-world/ui/src/globals.css';
+import SiteHeader from '@hey-world/components/src/site-header';
+import ThemeProvider from '@hey-world/components/src/theme-provider';
+import { ThemeScript, geistSans, geistMono } from '@hey-world/components';
+
+export const metadata: Metadata = {
+  title: 'Hey Blog!',
+  description: 'Need to think about this',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <ThemeScript />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} flex min-h-screen flex-col antialiased`}
+      >
+        <ThemeProvider>
+          <SiteHeader
+            title="Hey Blog!"
+            titleHref="/"
+            pages={[
+              { name: 'Blog', href: '/blog' },
+              { name: 'About', href: '/about' },
+              { name: 'Contact', href: '/contact' },
+            ]}
+          />
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ apps/blog/src/app/not-found.tsx
@@ -0,0 +1,5 @@
+import { NotFound } from '@hey-world/components';
+
+export default function NotFoundPage() {
+  return <NotFound />;
+}
