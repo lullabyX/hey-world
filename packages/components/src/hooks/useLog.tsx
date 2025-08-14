ebbc5e4be13d0286306f0671eb655dfcce09b642
lullabyX <hassan_rabbi@live.com>
2025-08-15 03:45:48 +0600

    log hook

--- packages/components/src/hooks/useLog.tsx
+++ packages/components/src/hooks/useLog.tsx
@@ -1,9 +1,19 @@
-import { useEffect } from 'react';
+'use client';
 
-const useLog = (message: string) => {
-  useEffect(() => {
-    console.log(message);
-  }, [message]);
+import { useRef } from 'react';
+
+const useLog = (delayInMs = 500) => {
+  const timeRef = useRef<number>(0);
+
+  const log: (...args: Parameters<typeof console.log>) => void = (...args) => {
+    const now = performance.now();
+    if (now - timeRef.current >= delayInMs) {
+      timeRef.current = now;
+      console.log(...args);
+    }
+  };
+
+  return log;
 };
 
 export default useLog;
