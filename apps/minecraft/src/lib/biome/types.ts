b6544e595551521f02d52bc246491cc9f85e4c64
lullabyX <hassan_rabbi@live.com>
2025-09-15 00:16:21 +0600

    add todo

---
+++ apps/minecraft/src/lib/biome/types.ts
@@ -0,0 +1,17 @@
+const Biome = {
+  jungle: {
+    color: '#00FF00',
+  },
+  tundra: {
+    color: '#0000FF',
+    texture: 'tundra',
+  },
+  desert: {
+    color: '#FF0000',
+    texture: 'desert',
+  },
+  meadow: {
+    color: '#00FFFF',
+    texture: 'meadow',
+  },
+};
